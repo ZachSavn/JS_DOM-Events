fe62b97b871b28e626f9b4bb0076b1da69cd62a2
zachary webb <zachsavn@gmail.com>
2022-03-31 23:49:06 -0400

    Finished exercise 3

--- main.js
+++ main.js
@@ -22,6 +22,14 @@ for (let ele of h3s){
 
 // TODO: Create a paragraph element using this document.createElement() and put this text inside "This node was created using the createElement() method"
 
+// TODO: Append the created node to the parent node using the element.appendChild() method
+
+// TODO: Create a <a> element using this document.createElement() and put this text inside "I am a <a> tag"
+
+// BONUS: Add a link href to the <a>
+
+// TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
+
 let parent = document.getElementById("parent")
 
 let p = document.createElement("p")//creates p element
@@ -36,27 +44,28 @@ parent.appendChild(p);//puts created p to DOM doc
 
 setTimeout(() => {
     parent.insertBefore(a , p);
-},4000);//sets 4 seconds
-
-
-
-// TODO: Append the created node to the parent node using the element.appendChild() method
-
-// TODO: Create a <a> element using this document.createElement() and put this text inside "I am a <a> tag"
-
-// BONUS: Add a link href to the <a>
-
-// TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
+},2000);//sets 2 seconds
 
 /*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
 
 // TODO: Replace the "Child Node" with a new <p> element that reads "New Child Node"
 
+let parent3 = document.getElementById("exercise-container3");
+
+let N1 = document.getElementById("N1");
+N1.textContent = "New Child Node";
+
 // TODO: Remove the "New Child Node"
+
+setTimeout(()=> {
+    parent3.removeChild(N1);
+}), 5000;
 
 /*----------- Exercise #4: ANIMATIONS ----------- */
 // TODO: Write your JavaScript here to make the red box go from left to right
 // let interval = setInterval(move, 10);
+
+
 
 // BONUS - Make the red box go all the way around the perimeter of the green box
 
